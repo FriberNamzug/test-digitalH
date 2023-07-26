d7e1f0b39ba732f485db16909e688a2d12441661
Roberto Friber <robertofriber@gmail.com>
2023-07-25 18:17:00 -0600

    Diseño base

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <div className='bg-gray-800 text-white text-center py-1'>
+            <p className="italic text-sm" >
+                Roberto Friber Marin Guzman - 25/07/2023
+            </p>
+        </div>
+    )
+}
